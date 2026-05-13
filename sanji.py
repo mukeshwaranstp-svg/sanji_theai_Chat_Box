bf9c055fb55b6290821c7d1086fb1be3076431d0
mukeshwaran <mukeshwaran.stp@gmail.com>
2026-05-13 19:55:27 +0530

    Mission 4: Sanji memory system complete

--- sanji.py
+++ sanji.py
@@ -1,4 +1,5 @@
 import os
+import json
 from groq import Groq
 from dotenv import load_dotenv
 
@@ -6,29 +7,62 @@ load_dotenv()
 
 client = Groq()
 
-SYSTEM_PROMPT = """You are Sanji, a sharp and efficient AI assistant.
+def load_memory():
+    if os.path.exists("sanji_memory.json"):
+        with open("sanji_memory.json", "r") as f:
+            sanji_memory = json.load(f)
+            return sanji_memory
+    else:
+        return[]
+
+sanji_memory = load_memory()
+
+SYSTEM_PROMPT = f"""You are Sanji, a sharp and efficient AI assistant.
 You're direct, intelligent, and slightly sarcastic — like a brilliant friend, not a corporate chatbot.
-Keep responses concise unless asked to elaborate."""
+Keep responses concise unless asked to elaborate.
+
+What you know about the user:
+{sanji_memory}"""
+
 
 conversation= []
 
+
+def save_memory(sanji_memory):
+    with open("sanji_memory.json", "w") as f:
+        json.dump(sanji_memory, f)
+
 def chat(user_input):
+    global sanji_memory
+
     conversation.append({"role": "user", "content": user_input})
     response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[{"role": "system", "content": SYSTEM_PROMPT}]+conversation
     )
     conversation.append({"role": "assistant", "content": response.choices[0].message.content})
+    
+    fact_check = client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        messages=[{"role": "system", "content": "You extract personal facts about the user from their message. If there is a personal fact, return only the fact as a short sentence. If there is no personal fact, return 'none' "},
+       { "role": "user", "content": user_input}]
+
+    )
+    fact = fact_check.choices[0].message.content
+    if fact != "none":
+        sanji_memory.append(fact)
+        save_memory(sanji_memory)
+    else:
+        pass
+
     return response.choices[0].message.content
+
+
 while True:
     user_input = input("you: ")
     if user_input.lower() in ["quit", "exit", "stop","bye"]:
-        with open("conversation_history.txt", "w") as f:
-            for message in conversation:
-                f.write(f"{message['role']}: {message['content']}\n")
-                f.write('_'*100 + '\n')
-        print("Conversation history saved to conversation_history.txt")
         print("Sanji;Alright, if you need any help,just type hellow baddy, i will be there")
         break
+
     sanji_response = chat(user_input)
     print(f"sanji:{sanji_response}")
